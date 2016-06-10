6415c7383b4456e0046c7c7131ae2555b9da2e00
Maxime Hermouet <maxime.hermouet@mobiapps.fr>
2016-06-10 16:39:59 +0200

    Creation de groupes dans un tournois

--- app/scripts/controllers/babitchTournament.js
+++ app/scripts/controllers/babitchTournament.js
@@ -1,32 +1,58 @@
 'use strict';
 
-angular.module('babitchFrontendApp').controller('babitchTournamentCtrl', function($scope, Restangular, $stateParams, $location) {
+angular.module('babitchFrontendApp').controller('babitchTournamentCtrl', function($scope, Restangular, $stateParams, $window) {
 
     $scope.tournament = {
         id: 0
     };
 
+    $scope.groups = [];
+
     Restangular.one('tournaments', $stateParams.id).get().then(function(data) {
-        // var teams = [];
-        // angular.forEach(data.teams, function(value, key) {
-        //   this.push(value.id);
-        // }, teams);
-        // data.teams = teams;
         $scope.tournament = data;
+        $scope.tournament.getList('groups').then(function(groups) {
+            $scope.groups = groups;
+        });
     });
 
-    // function to submit the form after all validation has occurred
-    // $scope.submitForm = function() {
+    $scope.deleteGroups = function() {
+        if($window.confirm('Are you sure you want to delete all groups from tournament ' + $scope.tournament.name +' ?')) {
+            angular.forEach($scope.groups, function(group, key) {
+                group.remove().then(function() {
+                    $scope.groups = _.without($scope.groups, group);
+                });
+            });
+        }
+    }
 
-    //     // check to make sure the form is completely valid
-    //     if ($scope.tournamentForm.$valid) {
-    //         Restangular.all('tournaments').post($scope.tournament).then(function() {
-    //             $location.path('/admin/tournaments');
-    //         });
-    //         return true;
-    //     }
-    //     else {
-    //         return false;
-    //     }
-    // };
+    $scope.generateGroups = function() {
+        if($window.confirm('Are you sure ?')) {
+            var groupA = {
+                name: 'Poule A',
+                tournament: $scope.tournament.id,
+                teams: []
+            };
+            var groupB = {
+                name: 'Poule B',
+                tournament: $scope.tournament.id,
+                teams: []
+            };
+
+            for(var i = 0; i < $scope.tournament.teams.length; i++) {
+                if ((i+1) <= $scope.tournament.teams.length/2) {
+                    groupA.teams.push($scope.tournament.teams[i].id);
+                } else {
+                    groupB.teams.push($scope.tournament.teams[i].id);
+                }
+            }
+
+            $scope.tournament.post('groups', groupA).then(function() {
+                $scope.tournament.post('groups', groupB).then(function() {
+                    $scope.tournament.getList('groups').then(function(groups) {
+                        $scope.groups = groups;
+                    });
+                });
+            });
+        }
+    };
 });
